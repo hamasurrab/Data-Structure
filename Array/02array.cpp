ff28262e61d20f4811331f063728501a0cd77807
hamasurrab <hamasurrab2@gmail.com>
2022-06-17 21:52:57 +0530

    02 array

---
+++ Array/02array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+
+// int a[100]={2};
+// int n;
+// cout<<"enter the valueof n"<<endl;
+// cin>>n;
+// // cout<<"_________________________________";
+// for (int i=0;i<n;i++){
+//     cin>>a[i];
+// }
+// cout<<"_________________________________"<<endl;
+// for(int i=0;i<100;i++){
+//     cout<<a[i]<<" "<<endl;
+// }
+
+
+}
