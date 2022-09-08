14f52be29d123835cc4a9a30ef229aeedc1e35ed
hamasurrab <hamasurrab2@gmail.com>
2022-09-09 01:34:03 +0530

    character array(run length)

---
+++ CharacterArrayString/run_length.cpp
@@ -0,0 +1,16 @@
+// run length encoding for compression
+
+#include<iostream>
+using namespace std;
+  
+string compressString(string str){
+
+}
+
+int main(){
+    string s1="aaabbccc";
+    cout<<compressString(s1)<<endl;
+
+    string s2="abcd";
+    cout<<compressString(s2)<<endl;
+}
