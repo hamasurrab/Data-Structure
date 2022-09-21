c7fb58f63b3103299962bf19148f3874d489785c
hamasurrab <hamasurrab2@gmail.com>
2022-09-21 22:58:33 +0530

    index.js

---
+++ 2D arrays/index.js
@@ -0,0 +1,14 @@
+prompt("Hey tell me your name?");
+prompt("Hey tell me your crush name?");
+var loveScore=Math.random()*100;
+loveScore=Math.floor(loveScore)+1;
+if(loveScore>60){
+    alert("your loveScore is "+loveScore+"%"+" all the best you got chance ");
+}
+if(loveScore>30 &&loveScore<=60){
+alert("ur loveScore is"+loveScore+"%");
+}
+else{
+    alert("you loveScore is "+loveScore+"%"+" sorry tRy with someone else");
+}
+
